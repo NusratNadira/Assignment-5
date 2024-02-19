339160ef2ed3f2587cd2e9b200cb5e38ff7c88da
NusratNadira <nusratnadira2@gmail.com>
2024-02-19 21:07:25 +0600

    upload

--- script.js
+++ script.js
@@ -8,14 +8,24 @@ function hideElements(elementId){
         element.classList.remove('hidden');
         }
     
-    
-     function setbackgroundColor(elementId){
-        const element = document.getElementById(elementId);
-        element.classList.add('bg-orange-400');
+        function getTextElementValueById(elementId){
+            const element = document.getElementById(elementId);
+            const value= element.innerText ;
+            return value;
+            }
+
+            function setTextElementValueById(elementId , value){
+            const element = document.getElementById(elementId);
+             element.innerText=value;
+            }
+
+     function setbackgroundColor(elementClass){
+        const element = document.getElementsByClassName(elementClass);
+        element.classList.add('bg-green-400');
      } 
      function removebackgroundColor(elementId){
        const element = document.getElementById(elementId);
-       element.classList.remove('bg-orange-400');
+       element.classList.remove('bg-green-400');
     } 
     function selectNow(){
        hideElements('home-screen');
@@ -25,4 +35,26 @@ function hideElements(elementId){
      function continueNow(){
         hideElements('successful-page');
         showElements('home-screen');
+     }
+
+     const allseat =document.getElementsByClassName('a');
+
+     
+
+     let count=0;
+let leftSeat = 8;
+     for(const seat of allseat){
+        seat.addEventListener('click', function(e){
+            setbackgroundColor(seat);
+count= count+1;
+leftSeat = leftSeat-1;
+setTextElementValueById('seat-count',count);
+setTextElementValueById('seat-remain' , leftSeat);
+
+if(count == 4){
+    alert("You can not select no more seat!");
+}
+
+//setbackgroundColor(seat);
+        })
      }
